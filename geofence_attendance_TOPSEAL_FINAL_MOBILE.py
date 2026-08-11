1e812d74af76ea33c9c4117adbc01249742610e1
rkdals0912-lang <rkdals0912@gmail.com>
2026-08-11 22:12:05 +0900

    Update geofence_attendance_TOPSEAL_FINAL_MOBILE.py

--- geofence_attendance_TOPSEAL_FINAL_MOBILE.py
+++ geofence_attendance_TOPSEAL_FINAL_MOBILE.py
@@ -118,7 +118,7 @@ def init_state():
 init_state()
 
 def api_url():
-    return str(secret("GOOGLE_APPS_SCRIPT_URL", "https://script.google.com/macros/s/AKfycbwzISTU5EIR_1oTgMrXfpDZSkJvhS6QJ_svhyh-A8olsJTP7swxfzlx13s6i665WAnp/exec")).strip()
+    return str(secret("GOOGLE_APPS_SCRIPT_URL", "https://script.google.com/macros/s/AKfycbz6h22RdQaRXqY6UrS4XhfTqEs1Njz0BkZmYBw_-58k_S8mkrgC8i2Eqe-TdHOEifrn/exec")).strip()
 
 def api_token():
     return str(secret("API_TOKEN", "topseal_api_2026_B4kP8xN2qL7mR5vT9sC3wH6j")).strip()
