615017dd29f41c68c38bbde6a09fc8097daa114a
rkdals0912-lang <rkdals0912@gmail.com>
2026-08-11 19:46:13 +0900

    Add logo image to geofence attendance admin page

--- geofence_attendance_admin.py
+++ geofence_attendance_admin.py
@@ -11,7 +11,13 @@ try:
 except ImportError:
     streamlit_geolocation = None
 
+col1, col2, col3 = st.columns([1, 1.2, 1])
 
+with col2:
+    st.image(
+        "topseal_logo.png",
+        use_container_width=True
+    )
 st.set_page_config(
     page_title="출퇴근 반경",
     page_icon="📍",
